7952a2702bf34c208f1a5dd858012494e14330f0
Anderson dos Santos Andrade <anderson.axie.escola1@gmail.com>
2023-05-18 10:05:44 -0300

    Ajustando o estilo CSS da Header

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "recipes/home.html", context={"name": "Anderson"})
+    return render(request, "recipes/pages/home.html", context={
+        "name": "Anderson"
+    })
