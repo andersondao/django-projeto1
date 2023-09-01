03f53ca82bffb3bbad0669819b51d65009ee3fc8
Anderson dos Santos Andrade <andersondao@gmail.com>
2023-09-01 17:26:34 -0300

    Registrando Category na admin do Django

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -13,6 +22,10 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-
-
+    cover = models.ImageField(upload_to="recipes/covers/%Y/%m/%d/")
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
