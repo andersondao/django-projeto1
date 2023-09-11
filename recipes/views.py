666c0823d16083fd603ce5b62f2cfeb8a5af1371
Anderson dos Santos Andrade <andersondao@gmail.com>
2023-09-11 10:47:13 -0300

    Usando o get_object_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from utils.recipes.factory import make_recipe
 
@@ -35,13 +35,10 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = (
-        Recipe.objects.filter(
-            pk=id,
-            is_published=True,
-        )
-        .order_by("-id")
-        .first()
+    recipe = get_object_or_404(
+        Recipe,
+        pk=id,
+        is_published=True,
     )
 
     return render(
