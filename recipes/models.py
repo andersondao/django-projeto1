c186d5d34ee68ff50489799c8a030295b266b1a7
Anderson dos Santos Andrade <andersondao@gmail.com>
2023-09-11 14:09:29 -0300

    Corrigindo erro de tipagem

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,7 @@
-from typing import Any
-
 from django.contrib.auth.models import User
 from django.db import models
+
+# from typing import Any
 
 
 class Category(models.Model):
@@ -28,7 +28,8 @@ class Recipe(models.Model):
         upload_to="recipes/covers/%Y/%m/%d/", blank=True, default=""
     )
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
+        Category,
+        on_delete=models.SET_NULL, null=True, blank=True, default=None
     )
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
