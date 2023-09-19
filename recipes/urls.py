7aae155b525b1197d731c0f4221b02097d5b3b28
Anderson dos Santos Andrade <andersondao@gmail.com>
2023-09-19 14:39:58 -0300

    Criando uma URL para Search

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("recipes/category/<int:category_id>/", views.category, name="category"),  # noqa: E501
     path("recipes/<int:id>/", views.recipe, name="recipe"),
+    path("recipes/search/", lambda request: ..., name="search")
 
 ]
