d911eefe83a04abb506007e46301fe0472fce2b7
Anderson dos Santos Andrade <andersondao@gmail.com>
2023-10-03 09:11:52 -0300

    Criando um formulário de login

--- authors/views.py
+++ authors/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 
 
 def register_view(request):
@@ -37,7 +37,10 @@ def register_create(request):
 
 def login_view(request):
 
-    return render(request, 'authors/pages/login.html')
+    return render(request, 'authors/pages/login.html', {
+        'form': LoginForm,
+        'form_action': 'authors:login_create'
+    })
 
 
 def login_create(request):
